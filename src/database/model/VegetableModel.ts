ae15e3737b08a52a62f29c4fd7a2630e32c66ad3
ovindu <ovindunambukara@gmail.com>
2021-08-04 19:53:14 +0530

    add more queries

--- src/database/model/VegetableModel.ts
+++ src/database/model/VegetableModel.ts
@@ -1,4 +1,4 @@
 import {model} from "mongoose";
 import {ProductModel, ProductSchema} from "./ProductModel";
 
-export const VegetableModel = model<ProductModel>("vegitables", ProductSchema);
+export const VegetableModel = model<ProductModel>("vegetables", ProductSchema);
--- src/graphql/resolvers/Product.ts
+++ src/graphql/resolvers/Product.ts
@@ -1,11 +1,48 @@
 import {IResolvers} from "graphql-tools";
 import {FoodModel} from "../../database/model/FoodModel";
+import {VegetableModel} from "../../database/model/VegetableModel";
+import {PharmacyModel} from "../../database/model/PharmacyModel";
+import {MeatModel} from "../../database/model/MeatModel";
+import {ElectronicModel} from "../../database/model/ElectronicsModel";
+import {FruitsModel} from "../../database/model/FruitsModel";
 
 export const productResolver: IResolvers = {
   Query: {
     getFoodProducts: async () => {
-      const all = await FoodModel.find();
-      return all
-    }
+      const foods = await FoodModel.find();
+      return {
+        category_name: "Food", products: foods
+      };
+    },
+    getVegetableProducts: async () => {
+      const vegetables = await VegetableModel.find();
+      return {
+        category_name: "Vegetables", products: vegetables
+      };
+    },
+    getPharmacyProducts: async () => {
+      const pharmacy = await PharmacyModel.find();
+      return {
+        category_name: "Pharmacy", products: pharmacy
+      };
+    },
+    getMeatProducts: async () => {
+      const meat = await MeatModel.find();
+      return {
+        category_name: "Meat", products: meat
+      };
+    },
+    getElectronicProducts: async () => {
+      const electronics = await ElectronicModel.find();
+      return {
+        category_name: "Electronics", products: electronics
+      };
+    },
+    getFruitProducts: async () => {
+      const fruit = await FruitsModel.find();
+      return {
+        category_name: "Fruits", products: fruit
+      };
+    },
   }
 }
