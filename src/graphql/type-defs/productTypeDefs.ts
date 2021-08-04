5699aa4f85d4c6fb01ac932ac3135f96a60a8212
ovindu <ovindunambukara@gmail.com>
2021-08-04 19:53:14 +0530

    add more queries

--- src/graphql/type-defs/productTypeDefs.ts
+++ src/graphql/type-defs/productTypeDefs.ts
@@ -2,7 +2,17 @@ import {gql} from "apollo-server-express"
 
 export const productTypeDefs = gql`
     type Query{
-      getFoodProducts: [Product!]!
+      getFoodProducts: Category!
+      getMeatProducts: Category!
+      getFruitProducts: Category!
+      getElectronicProducts: Category!
+      getPharmacyProducts: Category!
+      getVegetableProducts: Category!
+    }
+    
+    type Category {
+      category_name: String!
+      products: [Product!]!
     }
     
     type Product {
